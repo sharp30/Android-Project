00a71fcd35b484bbc486489c2ec96c9523217bb7
Ofir Shapira <ofirs2003@gmail.com>
2021-01-14 18:30:52 +0200

    added contest listview

--- App/app/src/main/java/com/App/Contests/MyContestsActivity.java
+++ App/app/src/main/java/com/App/Contests/MyContestsActivity.java
@@ -2,7 +2,12 @@ package com.App.Contests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.App.R;
 
 public class MyContestsActivity extends AppCompatActivity {
@@ -12,4 +17,7 @@ public class MyContestsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_contests);
     }
+
+
+
 }
