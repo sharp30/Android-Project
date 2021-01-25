cffee0798b598a8c549783910ec4c496f42a2c0e
Ofir Shapira <ofirs2003@gmail.com>
2021-01-25 12:26:08 +0200

    added details seekbar

--- App/app/src/main/java/com/App/MyDetailsActivity.java
+++ App/app/src/main/java/com/App/MyDetailsActivity.java
@@ -3,12 +3,19 @@ package com.App;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MyDetailsActivity extends AppCompatActivity {
 
+    SeekBar height;
+    SeekBar weight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_details);
+
+        height = (SeekBar)findViewById(R.id.sbHeight);
+        weight =(SeekBar)findViewById(R.id.sbWeight);
+
     }
 }
